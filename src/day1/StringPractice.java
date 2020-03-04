e8f43b03d953bbc7da76e5f9bad8cc3342ca5058
Emma Sanders <serenity3347@gmail.com>
2020-03-04 10:18:35 -0600

    commit message

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -3,5 +3,6 @@ package day1;
 public class StringPractice {
     public static void main(String[] args) {
         System.out.println("my first project yayyyyyy");
+        System.out.println("I am learning");
     }
 }
