74c9f823cdfff69ff7158def2f0201c51f106e6d
Emma Sanders <serenity3347@gmail.com>
2020-03-04 12:39:14 -0600

    new practice

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -5,6 +5,11 @@ public class LoopPractice {
         for (int i = 0; i <10 ; i++) {
             System.out.println(i);
 
+
+        }
+        for (int i = 10; i <1 ; i--) {
+
+            System.out.println(i);
         }
     }
 }
